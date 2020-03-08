af3c9dab51a2d140149e6b2c989d0255e25e2ab9
fmbah <felix.mbah@matchesfashion.com>
2020-03-08 21:38:38 +0000

    AMI baker stage added

--- app/tasks/apache.sh
+++ app/tasks/apache.sh
@@ -3,8 +3,12 @@ set -e
 
 echo '---- install Apache'
 
-DEBIAN_FRONTEND=noninteractive apt-get -y update
-DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
+#DEBIAN_FRONTEND=noninteractive apt-get -y update
+#DEBIAN_FRONTEND=noninteractive apt-get -y install apache2
+
+yum update -y 
+yum install nginx -y 
+ 
 
 cat > /var/www/html/index.html <<HERE
 Plain text FTW!
