16f6f4352c475870ea320517d9c2c4727358bb07
fmbah <felix.mbah@matchesfashion.com>
2020-03-09 13:13:29 +0000

    develop branch cutover

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_vpc" "main" {
-  cidr_block = "${var.vpc_main_cidr}"
+  cidr_block = "var.vpc_main_cidr"
 }
 
 resource "aws_vpc" "dmz" {
-  cidr_block = "${var.vpc_dmz_cidr}"
+  cidr_block = "var.vpc_dmz_cidr"
 }
 
-output "main_vpc_id" {
-  value = "${aws_vpc.main.id}"
-}
+// output "main_vpc_id" {
+//   value = "${aws_vpc.main.id}"
+// }
 
-output "dmz_vpc_id" {
-  value = "${aws_vpc.dmz.id}"
-}
+// output "dmz_vpc_id" {
+//   value = "${aws_vpc.dmz.id}"
+// }
